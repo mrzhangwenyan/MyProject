a3efe91bd942ac668d189e87a94b96260503c20a
zhangwenyan <zhangwenyan@yaok.com>
2018-03-16 08:58:56 +0800

    creat project

---
+++ PeiPeiProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PeiPeiProject
+//
+//  Created by zhangwenyan on 2018/3/16.
+//  Copyright © 2018年 www.zhangwenyan@travel.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
